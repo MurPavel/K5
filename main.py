d0d8a5dfbf432539367bd57b42506ca73ddf5a8c
PavMurk <polm.redmi@gmail.com>
2023-05-03 01:31:28 +0300

    122

--- main.py
+++ main.py
@@ -4,15 +4,35 @@ import os
 app = Flask(__name__)
 
 
-@app.route('/K5')
-def k5():
-    return render_template('index.html')
+@app.route('/K5-pOyNPP')
+def K5_pOyNPP():
+    return render_template('index.html', kod='K5-pOyNPP', n='1', name='Никита')
+
+
+@app.route('/K5-j4fmmm')
+def K5_j4fmmm():
+    return render_template('index.html', kod='K5-j4fmmm', n='2', name='Мохамед')
+
+
+@app.route('/K5-svOAzJ')
+def K5_svOAzJ():
+    return render_template('index.html', kod='K5-svOAzJ', n='3', name='София')
+
+
+@app.route('/K5-w3tuEw')
+def K5_w3tuEw():
+    return render_template('index.html', kod='K5-w3tuEw', n='4', name='Ярослав')
+
+
+@app.route('/K5-Yka8qg')
+def K5_Yka8qg():
+    return render_template('index.html', kod='K5-Yka8qg', n='5', name='Никита')
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'К5'
+    return render_template('n.html')
 
 
 if __name__ == '__main__':
